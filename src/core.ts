e3317c78af13e45da7bcc7b124e518f2d32f2476
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-08-05 08:26:31 +0800

    implemented importFile

--- src/core.ts
+++ src/core.ts
@@ -1,6 +1,9 @@
 import { db } from './db'
 import { detectMimeType } from './detect'
 import { createHash } from 'crypto'
+import { readFileSync } from 'fs'
+
+export type StoreResult = number | Promise<number>
 
 export type StoreContentOptions = {
   raw_data: Buffer
@@ -11,45 +14,88 @@ export function storeContent({
   raw_data,
   media_type,
   tags,
-}: StoreContentOptions): number | Promise<number> {
-  if (media_type) {
-    return storeContentHelper(raw_data, media_type, tags)
-  }
-  return detectMimeType(raw_data).then(result => {
-    return storeContentHelper(raw_data, result.mime, tags)
-  })
+}: StoreContentOptions): StoreResult {
+  const hash = hashContent(raw_data)
+  return useMediaType(
+    media_type,
+    raw_data,
+    db.transaction(media_type => {
+      const content_id = storeContentAndHash({
+        raw_data,
+        media_type,
+        hash,
+        inline_raw_data: true,
+      })
+      if (tags) storeTags(content_id, tags)
+      return content_id
+    }),
+  )
 }
 
-function storeContentHelper(
+export type ImportFileOptions = {
+  // full path or relative path
+  filename: string
+  media_type?: string
+  tags?: string[]
+}
+export function importFile({
+  filename,
+  media_type,
+  tags,
+}: ImportFileOptions): StoreResult {
+  const raw_data = readFileSync(filename)
+  const hash = hashContent(raw_data)
+  return useMediaType(
+    media_type,
+    raw_data,
+    db.transaction(media_type => {
+      const content_id = storeContentAndHash({
+        raw_data,
+        hash,
+        media_type,
+        inline_raw_data: false,
+      })
+      if (tags) storeTags(content_id, tags)
+      return content_id
+    }),
+  )
+}
+
+function useMediaType<T>(
+  media_type: string | undefined,
   raw_data: Buffer,
-  media_type: string,
-  tags?: string[],
-): number {
-  return db.transaction(() => {
-    const content_id = storeContentAndHash(raw_data, media_type)
-    tags?.forEach(tag => {
-      db.insert('tag', { tag, content_id })
-    })
-  })()
+  fn: (media_type: string) => T,
+): T | Promise<T> {
+  if (media_type) {
+    return fn(media_type)
+  }
+  return detectMimeType(raw_data).then(result => fn(result.mime))
+}
+
+function storeTags(content_id: number, tags: string[]) {
+  tags.forEach(tag => db.insert('tag', { tag, content_id }))
 }
 
 const select_hash_id = db.prepare(`
 select content_id from sha256
 where hash = ?
 `)
-function storeContentAndHash(raw_data: Buffer, media_type: string) {
-  const hash = hashContent(raw_data)
-
+function storeContentAndHash(args: {
+  raw_data: Buffer
+  media_type: string
+  hash: Buffer
+  inline_raw_data: boolean
+}): number {
   // check if this content already exists
-  const row = select_hash_id.get(hash)
+  const row = select_hash_id.get(args.hash)
   if (row) return row.content_id
 
   // store the content
   const content_id = db.insert('content', {
-    raw_data: raw_data,
-    mime_type_id: getMimeTypeId(media_type),
+    raw_data: args.inline_raw_data ? args.raw_data : null,
+    mime_type_id: getMimeTypeId(args.media_type),
   })
-  db.insert('sha256', { hash, content_id })
+  db.insert('sha256', { hash: args.hash, content_id })
 
   return content_id
 }
