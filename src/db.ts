90977dc3b6e4fe33e357cd9353b64a51e480b226
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-08-17 09:34:55 +0800

    support usage from other packages
    - update migrationsPath in createDB()
    - include migrations folder into npm dist

--- src/db.ts
+++ src/db.ts
@@ -3,10 +3,14 @@ import { DBInstance, toSafeMode } from 'better-sqlite3-schema'
 import { join } from 'path'
 
 export function createDB(path = join('data', 'sqlite3.db')): DBInstance {
+  const basedir = __filename.endsWith('.ts')
+    ? join(__dirname, '..') // src
+    : join(__dirname, '..', '..') // dist/cjs
+  const migrationsPath = join(basedir, 'migrations')
   const db = DB({
     path,
     migrate: {
-      migrationsPath: 'migrations',
+      migrationsPath,
       table: 'migrations',
       force: false,
     },
